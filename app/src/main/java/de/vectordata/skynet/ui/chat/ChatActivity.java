303c4896018c24c7f38d0de19af6fb8185fbebfd
Twometer <twometer@outlook.de>
2018-12-15 18:17:25 +0100

    New UI features and improvements

--- app/src/main/java/de/vectordata/skynet/ui/chat/ChatActivity.java
+++ app/src/main/java/de/vectordata/skynet/ui/chat/ChatActivity.java
@@ -1,7 +1,11 @@
 package de.vectordata.skynet.ui.chat;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.vanniktech.emoji.EmojiEditText;
 import com.vanniktech.emoji.EmojiPopup;
@@ -9,7 +13,9 @@ import com.vanniktech.emoji.EmojiPopup;
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import de.vectordata.libjvsl.util.cscompat.DateTime;
@@ -47,5 +53,38 @@ public class ChatActivity extends AppCompatActivity {
         emojiToggleButton.setOnClickListener(v -> popup.toggle());
         messageInput.setOnClickListener(v -> popup.dismiss());
 
+        setupActionBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_chat, menu);
+        return true;
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar == null) return;
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+        View customView = View.inflate(actionBar.getThemedContext(), R.layout.actionbar_chat, null);
+        ActionBar.LayoutParams layout = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(customView, layout);
+
+        ImageButton backButton = customView.findViewById(R.id.button_back);
+        backButton.setOnClickListener(v -> onBackPressed());
+
+        ImageView avatar = customView.findViewById(R.id.image_avatar);
+        TextView nickname = customView.findViewById(R.id.label_nickname);
+        TextView onlineState = customView.findViewById(R.id.label_online_state);
+        nickname.setText("Twometer");
+        onlineState.setText("online");
+
+        Toolbar parent = (Toolbar) customView.getParent();
+        parent.setPadding(0, 0, 0, 0);
+        parent.setContentInsetsAbsolute(0, 0);
     }
 }
