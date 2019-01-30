6da06f0ce8a45d34728a7699d2c159699973152c
Twometer <twometer@outlook.de>
2019-01-30 23:16:06 +0100

    Database improvements in chat activity

--- app/src/main/java/de/vectordata/skynet/ui/chat/ChatActivityDirect.java
+++ app/src/main/java/de/vectordata/skynet/ui/chat/ChatActivityDirect.java
@@ -13,6 +13,7 @@ import de.vectordata.libjvsl.util.cscompat.DateTime;
 import de.vectordata.skynet.R;
 import de.vectordata.skynet.data.Storage;
 import de.vectordata.skynet.data.model.Channel;
+import de.vectordata.skynet.data.model.ChannelMessage;
 import de.vectordata.skynet.data.model.ChatMessage;
 import de.vectordata.skynet.data.model.enums.ChannelType;
 import de.vectordata.skynet.ui.chat.recycler.MessageAdapter;
@@ -29,33 +30,49 @@ public class ChatActivityDirect extends ChatActivityBase {
 
     private Handler databaseHandler = Handlers.createOnThread("DatabaseThread");
 
+    List<MessageItem> messageItems;
+    private MessageAdapter adapter;
+
     @Override
     public void initialize() {
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         long channelId = getIntent().getLongExtra(EXTRA_CHANNEL_ID, 0);
+        long myAccountId = Storage.getSession().getAccountId();
         databaseHandler.post(() -> {
             directChannel = Storage.getDatabase().channelDao().getById(channelId);
-            if (directChannel != null) {
-                profileDataChannel = Storage.getDatabase().channelDao().getByType(directChannel.getOther(), ChannelType.PROFILE_DATA);
-                List<ChatMessage> messages = Storage.getDatabase().chatMessageDao().queryLast(channelId, 50);
+            if (directChannel == null) return; // This should not happen in production
+
+            profileDataChannel = Storage.getDatabase().channelDao().getByType(directChannel.getOther(), ChannelType.PROFILE_DATA);
+
+            List<ChatMessage> messages = Storage.getDatabase().chatMessageDao().queryLast(channelId, 50);
+            for (ChatMessage message : messages) {
+                ChannelMessage parent = Storage.getDatabase().channelMessageDao().getById(message.getChannelId(), message.getMessageId());
+                MessageState messageState = /* TODO */ MessageState.SENDING;
+                MessageSide messageSide = parent.getSenderId() == myAccountId ? MessageSide.RIGHT : MessageSide.LEFT;
+                messageItems.add(new MessageItem(message.getText(), parent.getDispatchTime(), messageState, messageSide));
             }
+
+            runOnUiThread(adapter::notifyDataSetChanged);
         });
 
-        List<MessageItem> items = new ArrayList<>();
-        items.add(MessageItem.newSystemMessage("YESTERDAY"));
-        items.add(new MessageItem("Hi", ago(16, 55, 0), MessageState.SEEN, MessageSide.RIGHT));
-        items.add(MessageItem.newSystemMessage("TODAY"));
-        items.add(new MessageItem("Eyy moin", ago(16, 45, 0), MessageState.SEEN, MessageSide.LEFT));
-        items.add(new MessageItem("Lass uns mal ne Runde zocken, ich will meine neue Grafikkarte ausprobieren \uD83D\uDE02", ago(16, 45, 0), MessageState.SEEN, MessageSide.RIGHT));
-        items.add(new MessageItem("Ne mann ich geh jetzt ins Bett, morgen dann", ago(16, 40, 0), MessageState.SEEN, MessageSide.LEFT));
-        items.add(new MessageItem("Mhh okay, ich kann aber morgen erst nachmittags.", ago(16, 39, 0), MessageState.SEEN, MessageSide.RIGHT));
-        items.add(new MessageItem("Jetzt könnte ich", ago(0, 59, 0), MessageState.SEEN, MessageSide.RIGHT));
-        items.add(new MessageItem("Okay komm online, jetzt bin ich am PC", ago(0, 39, 0), MessageState.SEEN, MessageSide.LEFT));
-        items.add(new MessageItem("Sehr gut", ago(0, 35, 0), MessageState.SENT, MessageSide.RIGHT));
+        messageItems = new ArrayList<>();
+        adapter = new MessageAdapter(messageItems);
 
-        recyclerView.setAdapter(new MessageAdapter(items));
+        // This is test data for UI demonstration purposes
+        messageItems.add(MessageItem.newSystemMessage("YESTERDAY"));
+        messageItems.add(new MessageItem("Hi", ago(16, 55, 0), MessageState.SEEN, MessageSide.RIGHT));
+        messageItems.add(MessageItem.newSystemMessage("TODAY"));
+        messageItems.add(new MessageItem("Eyy moin", ago(16, 45, 0), MessageState.SEEN, MessageSide.LEFT));
+        messageItems.add(new MessageItem("Lass uns mal ne Runde zocken, ich will meine neue Grafikkarte ausprobieren \uD83D\uDE02", ago(16, 45, 0), MessageState.SEEN, MessageSide.RIGHT));
+        messageItems.add(new MessageItem("Ne mann ich geh jetzt ins Bett, morgen dann", ago(16, 40, 0), MessageState.SEEN, MessageSide.LEFT));
+        messageItems.add(new MessageItem("Mhh okay, ich kann aber morgen erst nachmittags.", ago(16, 39, 0), MessageState.SEEN, MessageSide.RIGHT));
+        messageItems.add(new MessageItem("Jetzt könnte ich", ago(0, 59, 0), MessageState.SEEN, MessageSide.RIGHT));
+        messageItems.add(new MessageItem("Okay komm online, jetzt bin ich am PC", ago(0, 39, 0), MessageState.SEEN, MessageSide.LEFT));
+        messageItems.add(new MessageItem("Sehr gut", ago(0, 35, 0), MessageState.SENT, MessageSide.RIGHT));
+
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
