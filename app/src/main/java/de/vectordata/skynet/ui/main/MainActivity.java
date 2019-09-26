4d64eda5d6bb953ac99d11466eb081aa91477ff5
Twometer <twometer@outlook.de>
2019-09-26 19:08:37 +0200

    Updated to new domain

--- app/src/main/java/de/vectordata/skynet/ui/main/MainActivity.java
+++ app/src/main/java/de/vectordata/skynet/ui/main/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends ThemedActivity {
         }
 
         if (id == R.id.action_beta_info) {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.skynet-messenger.com/beta"));
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.skynet.app/beta"));
             startActivity(browserIntent);
             return true;
         }
