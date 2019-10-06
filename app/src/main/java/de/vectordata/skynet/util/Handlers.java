8a501057fd917e89f15114701c20b32e0e050649
Twometer <twometer@outlook.de>
2019-10-06 15:59:56 +0200

    Cache handler threads to improve performance and fix memory leak

--- app/src/main/java/de/vectordata/skynet/util/Handlers.java
+++ app/src/main/java/de/vectordata/skynet/util/Handlers.java
@@ -3,12 +3,23 @@ package de.vectordata.skynet.util;
 import android.os.Handler;
 import android.os.HandlerThread;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Handlers {
 
+    private static Map<String, Handler> cache = new HashMap<>();
+
     public static Handler createOnThread(String threadName) {
+        Handler handler = cache.get(threadName);
+        if (handler != null && handler.getLooper().getThread().isAlive())
+            return handler;
+
         HandlerThread thread = new HandlerThread(threadName);
         thread.start();
-        return new Handler(thread.getLooper());
+        handler = new Handler(thread.getLooper());
+        cache.put(threadName, handler);
+        return handler;
     }
 
 }
