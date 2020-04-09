c03860d1c5489314d99b06531a90b75d96f23f9c
Twometer <twometer@outlook.de>
2020-04-09 16:09:46 +0200

    Fix crash on overwriting device list

--- app/src/main/java/de/vectordata/skynet/data/sql/dao/DeviceListDao.java
+++ app/src/main/java/de/vectordata/skynet/data/sql/dao/DeviceListDao.java
@@ -2,6 +2,7 @@ package de.vectordata.skynet.data.sql.dao;
 
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import de.vectordata.skynet.data.model.Device;
@@ -9,7 +10,7 @@ import de.vectordata.skynet.data.model.Device;
 @Dao
 public interface DeviceListDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Device... devices);
 
     @Query("DELETE FROM deviceList")
