f5e87aec4e74a9f173ee1c51bd1de4b96ba98e49
Twometer <twometer@outlook.de>
2020-04-13 00:37:23 +0200

    Fix crash when creating profile image for empty nickname

--- app/src/main/java/de/vectordata/skynet/ui/util/DefaultProfileImage.java
+++ app/src/main/java/de/vectordata/skynet/ui/util/DefaultProfileImage.java
@@ -49,11 +49,13 @@ public class DefaultProfileImage {
         paint.setTextAlign(Paint.Align.CENTER);
         paint.setTextSize(68);
 
-        Rect textBounds = new Rect();
-        paint.getTextBounds(initials, 0, 1, textBounds);
-        int textHeight = Math.abs(textBounds.height());
+        if (!initials.isEmpty()) {
+            Rect textBounds = new Rect();
+            paint.getTextBounds(initials, 0, 1, textBounds);
+            int textHeight = Math.abs(textBounds.height());
 
-        canvas.drawText(initials, bitmap.getWidth() / 2f, bitmap.getHeight() / 2f + textHeight / 2f, paint);
+            canvas.drawText(initials, bitmap.getWidth() / 2f, bitmap.getHeight() / 2f + textHeight / 2f, paint);
+        }
         return new DefaultProfileImage(bitmap);
     }
 
